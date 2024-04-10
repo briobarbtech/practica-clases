da592b9a2ce35efc56ff2b91209009d0ed3ee8bf
Brian Barquesi <barquesi.tech@gmail.com>
2024-04-10 17:42:07 -0300

    agregando nuevos ejercicios

--- clase_2/bucles.js
+++ clase_2/bucles.js
@@ -1,4 +1,10 @@
+// Hacer que un programa muestre la cuenta regresiva desde un número "n" dado
+
+
 // Hacer que un código que agregue numeros randoms del 0 al 100
 
 
 // Hacer que el programa sume solo los números pares
+
+
+// Hacer que el programa muestre los primeros "n" t+erminos de la secuencia de fibonacci
