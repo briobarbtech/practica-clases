d07449f4cf5d0e814d6c70dda6e3d686a8583a60
Brian Barquesi <barquesi.tech@gmail.com>
2024-05-15 16:38:57 -0300

    adding exercise 1

---
+++ clase_4/encrypt.js
@@ -0,0 +1,7 @@
+// crear una constante con un array que contenga el abecedario (mayusculas y minusculas), un espacio, punto, coma, los signos de exclamación e interrogación, y los números naturales
+// crear una variable que contenga el mensaje
+// vamos a definir un ciclo for que recorra cada letra del mensaje
+// mientras lo recorre debe de buscar de cada letra su indice en el array abc
+// una vez encontrado lo vamos a almacenar en una variable
+// Ya tenemos el mensaje encriptado
+// Ahora crea una forma de desencriptarlo
