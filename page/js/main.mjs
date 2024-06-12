6c2a5b2425c5ca8484f164b44b5028bb3b1fc135
Brian Barquesi <barquesi.tech@gmail.com>
2024-06-12 13:52:51 -0300

    adding transitions to game-status

--- page/js/main.mjs
+++ page/js/main.mjs
@@ -2,6 +2,7 @@ import { getWord } from "./getWord.mjs";
 import { hideWord } from "./hideWord.mjs";
 import { verifyWord } from "./verifyWord.mjs";
 import { createItemList } from "./createItemList.mjs";
+import { winGame } from "./winGame.mjs";
 const wordElement = document.getElementById("content-word"); // Elemento que almacena la palabra
 const descElement = document.getElementById("content-desc"); // Elemento que almacena la descripción
 const userWord = document.getElementById("input-user"); // Input para el usuario
@@ -9,54 +10,43 @@ const btnVerify = document.getElementById("btn-submit");
 const state = document.getElementById("game-status");
 const btnPlayAgain = document.getElementById("btn-play-again");
 const ul = document.querySelector("#content-word #ul-word");
-// obtener palabra
-/* const word = getWord();
-wordElement.appendChild(createUnorderList(hideWord(word[0])));
-descElement.innerHTML = word[1]; */
+
+// AddEventListener
 btnVerify.addEventListener("click", () => {
-  console.log(userWord.value);
-  console.log(word[0]);
   if (verifyWord(word[0], userWord.value.trim())) {
-    winGame();
+    winGame(word)
   } else {
-    state.innerHTML = "Intenta otra vez";
+    state.classList.remove('disabled');
+    state.innerHTML = "Intenta otra vez"
     userWord.value = "";
+    setTimeout(()=>{
+      state.classList.add('disabled')
+    },3000)
   }
 });
 btnPlayAgain.addEventListener("click", () => {
   play();
 });
+
+// Palabra vacía
 let word = "";
 function play() {
   userWord.value = "";
   state.innerHTML = ""
   btnPlayAgain.classList.add("disabled");
   userWord.classList.remove("class", "disabled");
-  if (wordElement.classList.contains("disabled")) {
-    wordElement.classList.remove("class", "disabled");
-
-  }
-  if (btnVerify.classList.contains("disabled")) {
-    btnVerify.classList.remove("class", "disabled");
-  }
-  //state.classList.add("disabled");
+  if (wordElement.classList.contains("disabled")) wordElement.classList.remove("class", "disabled");
+  if (btnVerify.classList.contains("disabled")) btnVerify.classList.remove("class", "disabled");
+  
   word = getWord();
+
   const newUl = createItemList(hideWord(word[0]));
   
   ul.innerHTML = newUl;
-
   descElement.innerHTML = word[1];
+
   console.log(word[0]);
 }
-function winGame() {
-  // TODO: desactivar los elementos para terminar el juego
-  btnPlayAgain.classList.remove("class", "disabled"); 
-  state.innerHTML = `¡Ganaste! La palabra era: ${word[0]}`;
-  state.classList.remove("class","disabled");
-  wordElement.classList.add("disabled");
-  userWord.classList.add("disabled");
-  btnVerify.classList.add("disabled");
-  
-}
+
 
 play();
--- page/js/winGame.mjs
+++ page/js/winGame.mjs
@@ -1,4 +1,16 @@
-export function winGame(state) {
-    // TODO: desactivar los elementos para terminar el juego
-    state.innerHTML = "Ganaste";
-  }
+const btnPlayAgain = document.getElementById("btn-play-again");
+const state = document.getElementById("game-status");
+const wordElement = document.getElementById("content-word"); // Elemento que almacena la palabra
+const userWord = document.getElementById("input-user"); // Input para el usuario
+const btnVerify = document.getElementById("btn-submit");
+
+export function winGame(word) {
+  
+  btnPlayAgain.classList.remove("class", "disabled"); 
+  state.innerHTML = `¡Ganaste! La palabra era: ${word[0]}`;
+  state.classList.remove("class","disabled");
+  wordElement.classList.add("disabled");
+  userWord.classList.add("disabled");
+  btnVerify.classList.add("disabled");
+  
+}
